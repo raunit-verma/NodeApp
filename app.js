661f2d1ee0bdbdbd708b1bf6f0bd3a210522e5d6
Raunit Verma <raunit.verma@devtron.ai>
2024-01-25 18:37:36 +0530

    Second Commit

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 // File: server.js
 
 const express = require('express');
-const mongoose = require('mongoose');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://mongo:27017/admin';
-
-// Connect to MongoDB
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', (req, res) => {
     res.send('Hello from the backend!');
 });
+
+app.get('/', (req, res) => {
+    res.send("Home Route Working")
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
